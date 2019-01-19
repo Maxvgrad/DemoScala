ec208868357bf0e37dc77775b75c404fd01efb24
Maksim Vinogradov <maxvgrad@gmail.com>
2019-01-19 15:20:59 +0300

    lc8: String | Interpolators

---
+++ src/lectures/StringOps.scala
@@ -0,0 +1,31 @@
+package lectures
+
+object StringOps extends App {
+
+
+  val aNumberString = "42"
+  val aNumberLong = aNumberString.toLong
+  val aNumberDouble = aNumberString.toDouble
+  val aNumberFloat = aNumberString.toFloat
+
+
+  println('a' + aNumberString)
+  println('a' +: aNumberString)
+  println(aNumberString :+ 'b')
+
+  println(aNumberString.reverse)
+  println(aNumberString.take(1))
+
+  // S-interpolators
+
+  println(s"Float: $aNumberFloat")
+  println(s"Float + Long: ${aNumberFloat + aNumberLong}")
+
+  // F-interpolators
+
+  println(f"Float: $aNumberFloat%2.3f")
+
+
+  // R-intrpolator
+  println(raw"\t\t\t\n\\Hello")
+}
